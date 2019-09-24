fed3de3e0383cf82a7153b97508ce81b8aeffdb2
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-09-24 15:53:56 +1000

    ground work

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+use libhash::Hash as LibHash;
+use libsignature::Signature as LibSignature;
+use minisign::SignatureBox as MiniSignature;
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Signature<H>(MiniSignature, PhantomData<H>);
+
+impl<H> LibSignature for Signature<H>
+where
+    H: LibHash,
+{
+    type Hash = H;
+    type PublicKey = minisign::PublicKey;
+    type SecretKey = minisign::SecretKey;
+    type Error = minisign::PError;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
